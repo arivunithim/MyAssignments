29630f55b286eea0b18fa47b0a063a8afa32c5e8
arivunithim <42091282+arivunithim@users.noreply.github.com>
2024-07-16 21:04:46 +0530

    Add files via upload
    day2

---
+++ day2/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserversion = 91.0f;
+		String developertype = "Google";
+		boolean betatype = false;
+		int releaseyear = 2008;
+		char shortcut = 'c';
+		
+		System.out.println(browserversion);
+		System.out.println(developertype);
+		System.out.println(betatype);
+		System.out.println(releaseyear);
+		System.out.println(shortcut);
+		
+		
+
+	}
+
+}
---
+++ day2/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//datatypes variableName  value
+		
+		float browserversion = 100.2f;
+		String browsername = "firefox";
+		boolean isvisible = true;
+		int releaseyear = 1998;
+		char browserlogo = 'f';
+		
+		System.out.println(browserversion);
+		System.out.println(browsername);
+		System.out.println(isvisible);
+		System.out.println(releaseyear);
+		System.out.println(browserlogo);
+		
+		System.out.println(browserversion+""+browsername+""+isvisible+""+releaseyear+"\n"+browserlogo+"");
+	}
+
+}
