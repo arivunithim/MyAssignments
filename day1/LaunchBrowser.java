688ee0754435a752979a42a791c6465991f8cd06
arivunithim <42091282+arivunithim@users.noreply.github.com>
2024-08-19 18:02:07 +0530

    Add files via upload

---
+++ day1/LaunchBrowser.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		driver.close();
+		
+		
+		
+	}
+
+}
